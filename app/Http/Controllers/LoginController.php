abefdeb263eff28de8b6d065f564809354b261c4
Muhammad Ibrahim <islahboim@gmail.com>
2023-11-24 15:09:40 +0700

    login with api

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
@@ -19,22 +20,53 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
+    
+    /**
+     * Call the API endpoint for login.
+     */
+    protected function callApiLoginEndpoint(array $credentials)
+    {
+        // Adjust the URL to your actual API login endpoint
+        return \Http::post('https://api.ppdb.smktibazma.sch.id/api/v1/auth/login', $credentials);
+    }
+
+    /**
+     * Handle a login request to the application.
+     */
+
     public function login(Request $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
+        
+        $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $request->session()->regenerate();
+        // Call the API login endpoint
+        $response = $this->callApiLoginEndpoint($credentials);
 
-            return redirect()->intended('dashboard');
+        // Check if the API login was successful
+        if ($response->successful()) {
+            // Save the token in the session
+            $token = $response->json()['token'];
+            session(['token' => $token]);
+	
+            // $request->session()->regenerate();
+            // return redirect()->intended('dashboard');
+
+            // Redirect to the dashboard or return a JSON response
+            return $request->wantsJson()
+                ? response()->json(['message' => 'Login successful', 'token' => $token], 200)
+                : redirect('/dashboard');
+        } else {
+            dd('hallo');
+            exit();
+            // Handle login failure
+            return $request->wantsJson()
+                ? response()->json(['message' => 'Login failed. Please check your credentials.'], 401)
+                : back()->withErrors(['error' => 'Login failed. Please check your credentials.']);
         }
-
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
     }
 
     public function logout(Request $request)
